147bf3ab68fc31d68d5654a10b2ab0f317b9a0b5
云淡然 <cloudcome@163.com>
2015-01-15 12:29:23 +0800

    send error

--- index.js
+++ index.js
@@ -161,7 +161,7 @@ function _log(err, req, res, next) {
         }, function (err) {
             if (err) {
                 console.log('邮件发送失败：');
-                console.log(err.message);
+                console.error(err);
             }
         });
     }
